d9f89fa3851074fcb93f6138e5a2acd856016e49
HARSHITA <harshitasapra2005@gmail.com>
2025-03-23 18:40:09 +0530

    Add files via upload

---
+++ ARRAYSCLASS.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class ARRAYSCLASS {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,55};
+        int index=Arrays.binarySearch(arr,4);
+        System.out.println(index);
+
+    }
+}
